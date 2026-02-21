4fac80513ba4b0e27666044fdd3b869d7f5b8c7a
medhavi-cmd <medhavisingh270505@gmail.com>
2026-02-22 01:13:14 +0530

    started about and contact section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './sections/navbar.jsx'
 import './index.css'
 import Hero from './sections/Hero.jsx'
+import { Contact } from 'lucide-react'
 
 const App = () => {
   return (
    <div className='container mx-auto max-w-7xl'>
     <Navbar />
     <Hero />
+    <Contact />
    </div>
   )
 }
